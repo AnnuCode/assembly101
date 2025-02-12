b3042b3617763cdf97ea3a32871f96e952c4c221
Annu Singh <annu.singh@terramate.io>
2025-02-12 19:04:40 +0530

    using general address mode

--- largestvalueindex.s
+++ largestvalueindex.s
@@ -3,23 +3,27 @@
 numberofnumbers:
 .quad 7
 mynumbers:
-.quad 5,20,33,80,52,10,1
+.quad 4,3,7,23,55,34,62
 
 .section .text
 _start:
 movq numberofnumbers, %rcx
 movq $0, %rbx
 movq $0, %rdi
+
+// check before entering the loop
 cmp $0, %rcx
 je endloop
 
 myloop:
-movq mynumbers(,%rbx,8), %rax
+
+// put the next number from array to rax 
+movq mynumbers(,%rbx, 8), %rax
 cmp %rdi, %rax
 jbe loopcontrol
 movq %rax, %rdi
 
-loopcontrol:
+loopcontrol: 
 incq %rbx
 loopq myloop
 
